2d1220514aec1744e52b3e339ac12075bc274834
Tyler <lorenarcdev@gmail.com>
2024-01-13 13:47:26 -0500

    Written the foundation for this project

---
+++ constants.py
@@ -0,0 +1,9 @@
+WIDTH: int = 800
+HEIGHT: int = 600
+
+RESOLUTION: tuple = (WIDTH, HEIGHT)
+
+FPS: int = 60
+TARGET_FPS = FPS
+
+COLORS: dict = {"white": (255, 255, 255), "black": (0, 0, 0)}
---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame as pg
+import sys
+from constants import RESOLUTION, FPS, COLORS
+
+
+def main():
+    """This is the main script! Run this first to test the game!"""
+    pg.init()
+
+    window = pg.display.set_mode(RESOLUTION)
+    pg.display.set_caption("Crash Em Down")
+
+    canvas = pg.Surface(RESOLUTION)
+
+    clock = pg.time.Clock()
+    current_time: float = pg.time.get_ticks() / 1000
+    prev_time = current_time
+
+    def updateScene():
+        """This is where everything in the scene gets updated."""
+        pass
+
+    def drawScene():
+        """This is where everything in the scene gets updated."""
+        canvas.fill(COLORS["white"])
+
+        window.blit(canvas, (0, 0))
+        pg.display.flip()
+
+    # Game loop
+    while True:
+        clock.tick(FPS)
+
+        current_time: float = pg.time.get_ticks() / 1000
+        deltatime: float = current_time - prev_time
+
+        prev_time = current_time
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+        updateScene()
+        drawScene()
+
+
+if __name__ == "__main__":
+    main()
